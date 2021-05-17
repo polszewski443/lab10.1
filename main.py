bec6801bd31035d756b2be040b96cbed7b4d18f8
polszewski443 <70515373+polszewski443@users.noreply.github.com>
2021-05-17 11:45:24 +0200

    Update main.py

--- main.py
+++ main.py
@@ -4,10 +4,20 @@ import matplotlib.pyplot as plt
 
 x1 = np.arange(20, 41, 1)
 y1 = 1/x1
-plt.legend()
-plt.plot(x1, y1, 'r-', c='g')
+plt.plot(x1, y1, 'r-', c='purple')
+plt.legend(['linia wykresowa'])
 plt.xlabel('x')
 plt.ylabel('f(x)')
 plt.xlim([20,41])
+plt.ylim([0.02,0.05])
+plt.show()
+
+x1 = np.arange(20, 41, 1)
+y1 = 1/x1
+plt.plot(x1, y1, 'bo--')
+plt.title('Wykres funkcji f(x) dla x[20, 40]')
+plt.xlabel('x')
+plt.ylabel('1/x')
+plt.xlim([20,40])
 plt.ylim([0.02,0.05])
 plt.show()
